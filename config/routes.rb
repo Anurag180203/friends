0b2d95a7e3a6cd1f02334e676b69a23b67e2cfee
Anurag180203 <anurag180203@gmail.com>
2022-12-24 17:59:39 +0530

    refining app

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ end
   resources :friends
   # get 'home/index'
   get 'home/about'
-  root 'home#index'
+  # root 'home#index'
+  root 'friends#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
